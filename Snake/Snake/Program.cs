6e2fe4f67f37d04a04caa3c11bdb7d293ffc30e9
eweretti <eweretti@gmail.com>
2020-02-21 12:29:46 +0300

    Змейка двигается при нажатии клавиш

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -9,13 +10,39 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        { 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-           
+        {
+            //Отрисовка рамки
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4, 3, '#');
-            p2.Draw();
+            HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            topLine.DrawLine();
+            bottomLine.DrawLine();
+            leftLine.DrawLine();
+            rightLine.DrawLine();
+
+            //отрисовка точки
+            Point p = new Point(4, 5, '*');
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.DrawLine();
+            
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
 
             Console.ReadLine();
         }
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        Direction direction;
+
+        public Snake(Point tail, int lenght, Direction _direction)
+        {
+            direction = _direction;
+            pList = new List<Point>();
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tale = pList.First();
+            pList.Remove(tale);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tale.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
+    }
+}
